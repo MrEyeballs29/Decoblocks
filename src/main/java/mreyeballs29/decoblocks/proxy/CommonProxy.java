4af97fc6ff1062fb279e640bd2c05f56ab25b967
Joe Issac III <mreyeballs29@gmail.com>
2018-01-20 20:25:43 -0600

    Registered Porecelain Powder

--- src/main/java/mreyeballs29/decoblocks/proxy/CommonProxy.java
+++ src/main/java/mreyeballs29/decoblocks/proxy/CommonProxy.java
@@ -3,12 +3,14 @@ package mreyeballs29.decoblocks.proxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import mreyeballs29.decoblocks.block.ModBlocks;
 import mreyeballs29.decoblocks.item.ModItems;
 import mreyeballs29.decoblocks.recipes.ShapedCrafting;
 
 public class CommonProxy {
     public void preInit(FMLPreInitializationEvent event) {
     	ModItems.init();
+    	ModBlocks.init();
     }
 
     public void init(FMLInitializationEvent event) {
