0b9d1bedd034580a569db89537d9aca5846478b7
Joe Issac III <mreyeballs29@gmail.com>
2018-01-28 12:18:24 -0600

    Added Recipes for 16 Colors and Cream Block

--- src/main/java/mreyeballs29/decoblocks/recipes/ShapedCrafting.java
+++ src/main/java/mreyeballs29/decoblocks/recipes/ShapedCrafting.java
@@ -12,5 +12,22 @@ public final class ShapedCrafting {
 		GameRegistry.addRecipe(new ItemStack(ModItems.clayball, 4), new Object[]{"AB", "BA", 'A', Items.clay_ball, 'B', Blocks.dirt});
 		GameRegistry.addRecipe(new ItemStack(Blocks.sand), new Object[]{"AA", "AA", 'A', ModItems.sand});
 		GameRegistry.addRecipe(new ItemStack(ModBlocks.porcelainpowder), new Object[]{"AB", "BA", 'A', ModItems.sand, 'B', ModItems.clayball});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.cream), new Object[] {"AAA", "ABA", "AAA", 'A', Items.snowball, 'B', Items.milk_bucket});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.colorporcelain, 8, 0), new Object[] {"AAA", "ABA", "AAA", 'A', ModBlocks.porcelainblock, 'B', new ItemStack(Items.dye, 1, 15)});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.colorporcelain, 8, 1), new Object[] {"AAA", "ABA", "AAA", 'A', ModBlocks.porcelainblock, 'B', new ItemStack(Items.dye, 1, 14)});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.colorporcelain, 8, 2), new Object[] {"AAA", "ABA", "AAA", 'A', ModBlocks.porcelainblock, 'B', new ItemStack(Items.dye, 1, 13)});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.colorporcelain, 8, 3), new Object[] {"AAA", "ABA", "AAA", 'A', ModBlocks.porcelainblock, 'B', new ItemStack(Items.dye, 1, 12)});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.colorporcelain, 8, 4), new Object[] {"AAA", "ABA", "AAA", 'A', ModBlocks.porcelainblock, 'B', new ItemStack(Items.dye, 1, 11)});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.colorporcelain, 8, 5), new Object[] {"AAA", "ABA", "AAA", 'A', ModBlocks.porcelainblock, 'B', new ItemStack(Items.dye, 1, 10)});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.colorporcelain, 8, 6), new Object[] {"AAA", "ABA", "AAA", 'A', ModBlocks.porcelainblock, 'B', new ItemStack(Items.dye, 1, 9)});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.colorporcelain, 8, 7), new Object[] {"AAA", "ABA", "AAA", 'A', ModBlocks.porcelainblock, 'B', new ItemStack(Items.dye, 1, 8)});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.colorporcelain, 8, 8), new Object[] {"AAA", "ABA", "AAA", 'A', ModBlocks.porcelainblock, 'B', new ItemStack(Items.dye, 1, 7)});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.colorporcelain, 8, 9), new Object[] {"AAA", "ABA", "AAA", 'A', ModBlocks.porcelainblock, 'B', new ItemStack(Items.dye, 1, 6)});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.colorporcelain, 8, 10), new Object[] {"AAA", "ABA", "AAA", 'A', ModBlocks.porcelainblock, 'B', new ItemStack(Items.dye, 1, 5)});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.colorporcelain, 8, 11), new Object[] {"AAA", "ABA", "AAA", 'A', ModBlocks.porcelainblock, 'B', new ItemStack(Items.dye, 1, 4)});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.colorporcelain, 8, 12), new Object[] {"AAA", "ABA", "AAA", 'A', ModBlocks.porcelainblock, 'B', new ItemStack(Items.dye, 1, 3)});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.colorporcelain, 8, 13), new Object[] {"AAA", "ABA", "AAA", 'A', ModBlocks.porcelainblock, 'B', new ItemStack(Items.dye, 1, 2)});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.colorporcelain, 8, 14), new Object[] {"AAA", "ABA", "AAA", 'A', ModBlocks.porcelainblock, 'B', new ItemStack(Items.dye, 1, 1)});
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.colorporcelain, 8, 15), new Object[] {"AAA", "ABA", "AAA", 'A', ModBlocks.porcelainblock, 'B', new ItemStack(Items.dye, 1, 0)});
 	}
 }
--- src/main/java/mreyeballs29/decoblocks/recipes/ShapelessCrafting.java
+++ src/main/java/mreyeballs29/decoblocks/recipes/ShapelessCrafting.java
@@ -1,12 +1,30 @@
 package mreyeballs29.decoblocks.recipes;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import mreyeballs29.decoblocks.block.ModBlocks;
 import mreyeballs29.decoblocks.item.ModItems;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
 public final class ShapelessCrafting {
 	public static final void init() {
 		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.sand, 4), new Object[] {Blocks.sand});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colorporcelain, 1, 0), new Object[] {ModBlocks.porcelainblock, new ItemStack(Items.dye, 1, 15)});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colorporcelain, 1, 1), new Object[] {ModBlocks.porcelainblock, new ItemStack(Items.dye, 1, 14)});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colorporcelain, 1, 2), new Object[] {ModBlocks.porcelainblock, new ItemStack(Items.dye, 1, 13)});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colorporcelain, 1, 3), new Object[] {ModBlocks.porcelainblock, new ItemStack(Items.dye, 1, 12)});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colorporcelain, 1, 4), new Object[] {ModBlocks.porcelainblock, new ItemStack(Items.dye, 1, 11)});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colorporcelain, 1, 5), new Object[] {ModBlocks.porcelainblock, new ItemStack(Items.dye, 1, 10)});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colorporcelain, 1, 6), new Object[] {ModBlocks.porcelainblock, new ItemStack(Items.dye, 1, 9)});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colorporcelain, 1, 7), new Object[] {ModBlocks.porcelainblock, new ItemStack(Items.dye, 1, 8)});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colorporcelain, 1, 8), new Object[] {ModBlocks.porcelainblock, new ItemStack(Items.dye, 1, 7)});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colorporcelain, 1, 9), new Object[] {ModBlocks.porcelainblock, new ItemStack(Items.dye, 1, 6)});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colorporcelain, 1, 10), new Object[] {ModBlocks.porcelainblock, new ItemStack(Items.dye, 1, 5)});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colorporcelain, 1, 11), new Object[] {ModBlocks.porcelainblock, new ItemStack(Items.dye, 1, 4)});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colorporcelain, 1, 12), new Object[] {ModBlocks.porcelainblock, new ItemStack(Items.dye, 1, 3)});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colorporcelain, 1, 13), new Object[] {ModBlocks.porcelainblock, new ItemStack(Items.dye, 1, 2)});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colorporcelain, 1, 14), new Object[] {ModBlocks.porcelainblock, new ItemStack(Items.dye, 1, 1)});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.colorporcelain, 1, 15), new Object[] {ModBlocks.porcelainblock, new ItemStack(Items.dye, 1, 0)});
 	}
 }
