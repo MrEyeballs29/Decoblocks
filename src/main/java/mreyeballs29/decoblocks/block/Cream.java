98651ac99174802c399561cacf39b1616175c90f
Joe Issac III <mreyeballs29@gmail.com>
2018-01-28 12:16:33 -0600

    Added Cream Block

---
+++ src/main/java/mreyeballs29/decoblocks/block/Cream.java
@@ -0,0 +1,25 @@
+package mreyeballs29.decoblocks.block;
+
+import mreyeballs29.decoblocks.Constants;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.oredict.OreDictionary;
+
+public class Cream extends Block {
+
+	protected Cream(Material Material, String Name) {
+		super(Material);
+		this.setBlockName(Name);
+		this.setBlockTextureName(Constants.TEXTURE_PREFIX + Name);
+		this.setCreativeTab(CreativeTabs.tabBlock);
+		this.setHardness(0.4F);
+		this.setResistance(2.0F);
+		this.setStepSound(soundTypeSnow);
+		this.setHarvestLevel("shovel", 1);
+	}
+
+	public void addOreDict() {
+		OreDictionary.registerOre("blockCream", this);
+	}
+}
