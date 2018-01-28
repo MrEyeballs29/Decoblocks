c03553c6a89fd5725fe1288dc37d7092ac0c70ae
Joe Issac III <mreyeballs29@gmail.com>
2018-01-28 11:08:58 -0600

    Added Names

---
+++ src/main/java/mreyeballs29/decoblocks/libs/Names.java
@@ -0,0 +1,13 @@
+package mreyeballs29.decoblocks.libs;
+
+public class Names {
+	// Blocks
+	public static final String PORCLEAIN_DUST = "porcelain_dust";
+	public static final String PORCLEAIN_BLOCK = "porcleain_block";
+	public static final String CREAM = "cream";
+	public static final String COLOR_PORCLEAIN = "color_porcleain";
+	
+	// Items
+	public static final String CLAY_ROUND = "clay_round";
+	public static final String SAND_DUST = "sand_dust";
+}
