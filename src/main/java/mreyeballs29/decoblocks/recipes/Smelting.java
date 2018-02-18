024b679b2cdb92a78b27df512cc1a09554f399db
Joe Issac III <mreyeballs29@gmail.com>
2018-02-17 19:51:34 -0600

    Added Some Recipes

--- src/main/java/mreyeballs29/decoblocks/recipes/Smelting.java
+++ src/main/java/mreyeballs29/decoblocks/recipes/Smelting.java
@@ -8,6 +8,9 @@ import net.minecraft.item.ItemStack;
 public final class Smelting {
 	public static final void init() {
 		GameRegistry.addSmelting(ModBlocks.porcelainpowder, new ItemStack(ModBlocks.porcelainblock, 1), 0.1F);
-		GameRegistry.addSmelting(new ItemStack(Blocks.stonebrick, 1, 0), new ItemStack(ModBlocks.specialstone, 1, 0), 0.1F);
+		GameRegistry.addSmelting(new ItemStack(Blocks.stone, 1, 0), new ItemStack(ModBlocks.specialstone, 1, 0), 0.1F);
+		for (int i = 1; i < 8; i++) {
+			GameRegistry.addSmelting(new ItemStack(ModBlocks.specialstone, 1, i), new ItemStack(ModBlocks.specialstone, 1, 0), 0.1F);
+		}
 	}
 }
