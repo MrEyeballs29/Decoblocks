a01649653ba2a4262abe4d2778eaf1ae7ca6e7c4
Joe Issac III <mreyeballs29@gmail.com>
2018-03-04 11:06:50 -0600

    Released 1.2.0-R3

--- src/main/java/mreyeballs29/decoblocks/Constants.java
+++ src/main/java/mreyeballs29/decoblocks/Constants.java
@@ -2,7 +2,7 @@ package mreyeballs29.decoblocks;
 
 public class Constants {
 	public static final String modID = "decoblock";
-	public static final String version = "1.2.0-R2";
+	public static final String version = "1.2.0-R3";
 	public static final String modName = "Decoblocks";
 	public static final String TEXTURE_PREFIX = "decoblock:";
 }
