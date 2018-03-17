180ac9a6b9d366f3bf01947135fc4ca631e82702
Joe Issac III <mreyeballs29@gmail.com>
2018-03-17 12:56:34 -0500

    Added GC Support

--- src/main/java/mreyeballs29/decoblocks/Constants.java
+++ src/main/java/mreyeballs29/decoblocks/Constants.java
@@ -5,4 +5,7 @@ public class Constants {
 	public static final String version = "1.2.0-R3";
 	public static final String modName = "Decoblocks";
 	public static final String TEXTURE_PREFIX = "decoblock:";
+	
+	public static final String Intergration = "after:";
+	public static final String Dependency = "required-after:";
 }
--- src/main/java/mreyeballs29/decoblocks/Decoblocks.java
+++ src/main/java/mreyeballs29/decoblocks/Decoblocks.java
@@ -1,5 +1,6 @@
 package mreyeballs29.decoblocks;
 
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -10,18 +11,21 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import mreyeballs29.decoblocks.proxy.CommonProxy;
 import mreyeballs29.decoblocks.Constants;
 
-@Mod(modid = Constants.modID, name = Constants.modName, version = Constants.version)
+@Mod(modid = Constants.modID, name = Constants.modName, version = Constants.version, useMetadata = true, dependencies = "after:GalacticraftMars; after:GalacticraftCore")
 public class Decoblocks {
 	
 	@SidedProxy(clientSide="mreyeballs29.decoblocks.proxy.ClientProxy", serverSide="mreyeballs29.decoblocks.proxy.ServerProxy")
 	public static CommonProxy proxy;
 	
-    @Instance
+    @Instance(Constants.modID)
     public static Decoblocks instance = new Decoblocks();
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		proxy.preInit(event);
+		if(Loader.isModLoaded("GalacticraftMars") || Loader.isModLoaded("GalacticraftCore")) {
+			System.out.println("Time to Add Some Space Blocks and Items");
+		}
 	}
 	
 	@EventHandler
