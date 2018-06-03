26a878e1f3aa26d8d08dae38ab84649948d5ea2e
Joe Issac III <mreyeballs29@gmail.com>
2018-06-03 14:26:46 -0500

    Changed Form Resusable into Tool

---
+++ src/main/java/mreyeballs29/decoblocks/item/ItemTool.java
@@ -0,0 +1,28 @@
+package mreyeballs29.decoblocks.item;
+
+import mreyeballs29.decoblocks.Constants;
+import mreyeballs29.decoblocks.block.ModBlocks;
+import mreyeballs29.decoblocks.libs.Names;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class ItemTool extends Item {
+	private Boolean is3D;
+
+	public ItemTool(String name, Boolean is3d) {
+		this.setUnlocalizedName("tool" + Names.TitleCase(name));
+		this.setTextureName(Constants.TEXTURE_PREFIX + name);
+		this.setCreativeTab(ModBlocks.TabDecoClassic);
+		this.setMaxStackSize(1);
+		this.setContainerItem(this);
+		this.is3D = is3d;
+		if (this.is3D) {
+			this.setFull3D();
+		}
+	}
+
+	@Override
+	public boolean doesContainerItemLeaveCraftingGrid(ItemStack itemstack) {
+		return false;
+	}
+}
