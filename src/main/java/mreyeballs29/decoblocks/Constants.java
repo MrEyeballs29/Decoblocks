241bf9c97d1845cffdb068cf9dc459bd337e94e7
Joe Issac III <mreyeballs29@gmail.com>
2018-08-09 20:38:35 -0500

    Released 1.4.0 Final

--- src/main/java/mreyeballs29/decoblocks/Constants.java
+++ src/main/java/mreyeballs29/decoblocks/Constants.java
@@ -8,7 +8,7 @@ import net.minecraft.item.ItemStack;
 
 public class Constants {
 	public static final String modID = "decoblock";
-	public static final String version = "1.3.0 Final";
+	public static final String version = "1.4.0 Final";
 	public static final String modName = "Decoblocks";
 	public static final String TEXTURE_PREFIX = "decoblock:";
 	
