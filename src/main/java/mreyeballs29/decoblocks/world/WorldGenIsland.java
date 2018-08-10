27276e96db84bdedf6c0f2aa56af482ce0278d33
Joe Issac III <mreyeballs29@gmail.com>
2018-08-09 20:10:25 -0500

    Added Two Types of Generation

---
+++ src/main/java/mreyeballs29/decoblocks/world/WorldGenIsland.java
@@ -0,0 +1,16 @@
+package mreyeballs29.decoblocks.world;
+
+import java.util.Random;
+
+import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenerator;
+
+public class WorldGenIsland extends WorldGenerator {
+
+	@Override
+	public boolean generate(World p_76484_1_, Random random, int p_76484_3_, int p_76484_4_, int p_76484_5_) {
+		float f = random.nextInt();
+		return false;
+	}
+
+}
